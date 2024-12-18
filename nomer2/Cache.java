63265b5bcef2a19372a9093fbb8e427578df510a
LeonidElkov <148034601+LeonidElkov@users.noreply.github.com>
2024-12-19 00:51:09 +0500

    Create Cache.java

---
+++ nomer2/Cache.java
@@ -0,0 +1,14 @@
+package nomer2;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// Определяем аннотацию @Cache
+@Retention(RetentionPolicy.RUNTIME) // Доступна во время выполнения
+@Target(ElementType.TYPE) // Можем применять только к типам (классам)
+public @interface Cache {
+    // Необязательное свойство value типа String[]
+    String[] value() default {}; // Значение по умолчанию - пустой массив
+}
