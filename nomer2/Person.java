15fac73859db6100b4530b96a9f60a8faca24796
LeonidElkov <148034601+LeonidElkov@users.noreply.github.com>
2024-12-19 03:10:11 +0500

    Update Person.java

--- nomer2/Person.java
+++ nomer2/Person.java
@@ -1,42 +1,21 @@
 package nomer2;
 //exampletostring
-@ToString(value = "YES") // Применяем аннотацию к классу
-public class Person {
 
-    @ToString(value = "NO") // Применяем аннотацию к полю
+@ToString(value = "YES")
+public class Person { // Аннотируем класс
+    @ToString(value = "NO") // Аннотируем поле
     private String name;
 
     private int age;
 
-    // Конструктор
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    // Геттеры
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
     // Переопределяем метод toString
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Person{");
-        if (this.getClass().isAnnotationPresent(ToString.class)) {
-            ToString toStringAnnotation = this.getClass().getAnnotation(ToString.class);
-            if ("YES".equals(toStringAnnotation.value())) {
-                sb.append("name='").append(name).append('\'');
-                sb.append(", age=").append(age);
-            }
-        }
-        sb.append('}');
-        return sb.toString();
+        return "Person{" + "Имя = '" + name + '\'' + ", возраст = " + age + '}';
     }
 }
-
